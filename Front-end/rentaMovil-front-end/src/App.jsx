d5808231a1f8e816d472a17b250187b7116dd54f
david-erez <erezsantiago25@gmail.com>
2026-03-24 16:07:05 -0500

    cambio en jsn

--- Front-end/rentaMovil-front-end/src/App.jsx
+++ Front-end/rentaMovil-front-end/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  return <h1>RentaMóvil 🚗</h1>;
+  return <h1>RentaMóvil </h1>;
 }
 
 export default App;
