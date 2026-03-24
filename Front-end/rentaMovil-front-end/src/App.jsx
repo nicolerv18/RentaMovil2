d19e4533d3a23873f0458da2e9fff7c2d9d42f17
Thiago <thiagorojasg451@gmail.com>
2026-03-24 15:55:03 -0500

    Creacion de componentes y paginas

--- Front-end/rentaMovil-front-end/src/App.jsx
+++ Front-end/rentaMovil-front-end/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   return <h1>RentaMóvil 🚗</h1>;
 }
-
 export default App;
