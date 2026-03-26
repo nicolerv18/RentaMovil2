8ca5d0063dc1bcbd57eb617ab69437acf9a09912
nicolerv18 <nicolerv18007@gmail.com>
2026-03-26 17:05:51 -0500

    Agregar Footer  y navbar

--- Front-end/rentaMovil-front-end/src/App.jsx
+++ Front-end/rentaMovil-front-end/src/App.jsx
@@ -1,7 +1,16 @@
-
 import './App.css'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <h1>RentaMóvil </h1>;
+  return(
+    <BrowserRouter>
+      <Navbar/>
+      <Footer/>
+      
+    </BrowserRouter>
+  );
 }
+
 export default App;
