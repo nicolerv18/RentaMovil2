bd7bb62041498c06db036b69b728686e541da93b
Thiago <thiagorojasg451@gmail.com>
2026-04-09 14:07:18 -0500

    Delete

--- Front-end/rentaMovil-front-end/src/App.jsx
+++ Front-end/rentaMovil-front-end/src/App.jsx
@@ -14,7 +14,6 @@ import Pago from "./pages/page";
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
       {/* Navegación */}
       <nav>
         <Link to="/pago">Pago</Link>
@@ -30,7 +29,6 @@ function App() {
         <Route path="/pago" element={<Pago/>} />
       </Routes>
 
-      <Footer />
     </BrowserRouter>
   );
 }
